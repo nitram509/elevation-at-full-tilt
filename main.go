8eaa8b303ab7f3e6d16c99cd56aa646ba700e6c8
nitram509 <maki@bitkings.de>
2015-08-03 23:55:25 +0200

    fixed rounding issue by adjusting borders

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 const aFile string = "test-data/N50E014.hgt"
-const NO_OF_LINES = float64(1201)
+const NO_OF_LINES = int32(1201)
 
 type ball struct {
 	quadtree.Handle
@@ -33,29 +33,23 @@ func ExampleBalls() {
 	}
 	list := tree.FindNearObjects(quadtree.Twof{0.5, 0.1}, 0.2)
 	fmt.Println("Found", len(list))
-	// Output: Found 3
 }
 
 func getElevation(lat float64, lon float64) int16 {
-	fmt.Printf("Lat/Lon: %f, %f\n", lat,lon)
+	fmt.Printf("Lat/Lon: %f, %f\n", lat, lon)
 	f, err := os.Open(aFile)
 	check(err)
 
-	/*
-			<BoundingRectangle>
-				<WestBoundingCoordinate>13.99916667</WestBoundingCoordinate>
-				<NorthBoundingCoordinate>51.00083333</NorthBoundingCoordinate>
-				<EastBoundingCoordinate>15.00083333</EastBoundingCoordinate>
-				<SouthBoundingCoordinate>49.99916667</SouthBoundingCoordinate>
-			</BoundingRectangle>
-*/
-	upperLeft := quadtree.Twof{13.99916667, 51.00083333}
-	lowerRight := quadtree.Twof{15.00083333, 49.99916667}
+	upperLeft := quadtree.Twof{13.9995833, 51.0004167}
+	lowerRight := quadtree.Twof{15.0004167, 49.9995833}
+
 	dLat := math.Abs(lat - upperLeft[1])
 	dLon := math.Abs(lon - upperLeft[0])
 	distLat := math.Abs(upperLeft[1] - lowerRight[1])
 	distLon := math.Abs(upperLeft[0] - lowerRight[0])
-	offset := int64((dLat * float64(NO_OF_LINES) / distLat) + NO_OF_LINES * dLon * float64(NO_OF_LINES) / distLon) << 1
+	nearestLat := int32(dLat * float64(NO_OF_LINES) / distLat);
+	nearestLon := int32(dLon * float64(NO_OF_LINES) / distLon)
+	offset := int64(NO_OF_LINES * nearestLat + nearestLon) << 1
 	f.Seek(offset, 0)
 	check(err)
 	r4 := bufio.NewReader(f)

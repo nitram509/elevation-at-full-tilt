ca81d18b95c7fa429b516a65b3e8d8cb33392cc9
nitram509 <maki@bitkings.de>
2015-08-04 23:29:27 +0200

    using MetaData now

--- elevation.go
+++ elevation.go
@@ -8,14 +8,17 @@ import (
 )
 
 const aFile string = "test-data/N50E014.hgt"
+const aXmlFile string = "test-data/N50E014.hgt.xml"
 const NO_OF_PIXELS_PER_LINE = 1201
 
 func getElevation(lat float64, lon float64) int16 {
 
 	f, err := os.Open(aFile)
 	check(err)
+	defer f.Close()
 
-	upperLeft, lowerRight := calculateUpperLeftAndLowerRightLikeGdalDataSet()
+	boundingRectangle, _ := readBoundingRectangle(aXmlFile)
+	upperLeft, lowerRight := calculateUpperLeftAndLowerRightLikeGdalDataSet(boundingRectangle)
 
 	dLat := math.Abs(lat - upperLeft[1])
 	dLon := math.Abs(lon - upperLeft[0])
@@ -32,14 +35,14 @@ func getElevation(lat float64, lon float64) int16 {
 	return int16(int(heightBuf[0]) << 8 + int(heightBuf[1]))
 }
 
-func calculateUpperLeftAndLowerRightLikeGdalDataSet() (quadtree.Twof, quadtree.Twof) {
-	wbc := 13.99916667
-	nbc := 51.00083333
-	ebc := 15.00083333
-	sbc := 49.99916667
-
-	upperLeft := quadtree.Twof{wbc + 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1), nbc - 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1)}
-	lowerRight := quadtree.Twof{ebc - 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1), sbc + 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1)}
-
+func calculateUpperLeftAndLowerRightLikeGdalDataSet(boundingRectangle BoundingRectangle) (quadtree.Twof, quadtree.Twof) {
+	upperLeft := quadtree.Twof{
+		boundingRectangle.WestBoundingCoordinate + 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1),
+		boundingRectangle.NorthBoundingCoordinate - 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1),
+	}
+	lowerRight := quadtree.Twof{
+		boundingRectangle.EastBoundingCoordinate - 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1),
+		boundingRectangle.SouthBoundingCoordinate + 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1),
+	}
 	return upperLeft, lowerRight
 }

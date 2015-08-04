b8f4276bed6e300a8d7f9ff7f071e25fd88d871d
nitram509 <maki@bitkings.de>
2015-08-04 22:21:34 +0200

    solved calculation with magic numbers, like GDAL tool  see http://code.metager.de/source/xref/lib/gdal/frmts/srtmhgt/srtmhgtdataset.cpp

--- elevation.go
+++ elevation.go
@@ -9,23 +9,22 @@ import (
 )
 
 const aFile string = "test-data/N50E014.hgt"
-const NO_OF_LINES = int32(1201)
+const NO_OF_PIXELS_PER_LINE = 1201
 
 func getElevation(lat float64, lon float64) int16 {
 	fmt.Printf("Lat/Lon: %f, %f\n", lat, lon)
 	f, err := os.Open(aFile)
 	check(err)
 
-	upperLeft := quadtree.Twof{13.9995833, 51.0004167}
-	lowerRight := quadtree.Twof{15.0004167, 49.9995833}
+	upperLeft, lowerRight := calculateUpperLeftAndLowerRightLikeGdalDataSet()
 
 	dLat := math.Abs(lat - upperLeft[1])
 	dLon := math.Abs(lon - upperLeft[0])
 	distLat := math.Abs(upperLeft[1] - lowerRight[1])
 	distLon := math.Abs(upperLeft[0] - lowerRight[0])
-	nearestLat := int32(dLat * float64(NO_OF_LINES) / distLat);
-	nearestLon := int32(dLon * float64(NO_OF_LINES) / distLon)
-	offset := int64(NO_OF_LINES * nearestLat + nearestLon) << 1
+	nearestLat := int(dLat * float64(NO_OF_PIXELS_PER_LINE) / distLat);
+	nearestLon := int(dLon * float64(NO_OF_PIXELS_PER_LINE) / distLon)
+	offset := int64(NO_OF_PIXELS_PER_LINE * nearestLat + nearestLon) << 1
 	f.Seek(offset, 0)
 	check(err)
 	r4 := bufio.NewReader(f)
@@ -36,4 +35,16 @@ func getElevation(lat float64, lon float64) int16 {
 	fmt.Printf("Height-hex: %.2x%.2x\n", heightBuf[0], heightBuf[1])
 
 	return int16(int(heightBuf[0]) << 8 + int(heightBuf[1]))
+}
+
+func calculateUpperLeftAndLowerRightLikeGdalDataSet() (quadtree.Twof, quadtree.Twof) {
+	wbc := 13.99916667
+	nbc := 51.00083333
+	ebc := 15.00083333
+	sbc := 49.99916667
+
+	upperLeft := quadtree.Twof{wbc + 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1), nbc - 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1)}
+	lowerRight := quadtree.Twof{ebc - 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1), sbc + 0.5 / float64(NO_OF_PIXELS_PER_LINE - 1)}
+
+	return upperLeft, lowerRight
 }

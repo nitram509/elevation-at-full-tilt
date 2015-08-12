b884030f3c50e1f16116a39e860214896f0c8d14
nitram509 <maki@bitkings.de>
2015-08-12 09:47:00 +0200

    fixed compiler error

--- storage.go
+++ storage.go
@@ -36,8 +36,8 @@ func visit(path string, f os.FileInfo, err error) error {
 			srtmTile.CompressedData, err = ioutil.ReadFile(path)
 			check(err)
 			upperLeft, lowerRight := calculateUpperLeftAndLowerRightLikeGdalDataSet(srtmTile.BoundingRectangle)
-			srtmTileCollector.tree.Add(srtmTile, upperLeft)
-			srtmTileCollector.tree.Add(srtmTile, lowerRight)
+			srtmTileCollector.tree.Add(&srtmTile, upperLeft)
+			srtmTileCollector.tree.Add(&srtmTile, lowerRight)
 			srtmTileCollector.loadedDataSize += int64(len(srtmTile.CompressedData))
 			fmt.Printf("Visited: %s\n", path)
 			fmt.Printf(">> loadedDataSize: %d\n", srtmTileCollector.loadedDataSize)
@@ -47,11 +47,11 @@ return nil
 }
 
 
-func (collector SrtmTileCollector) init() {
-	collector.loadedDataSize = 0
+func (srtmTileCollector SrtmTileCollector) init() {
+	srtmTileCollector.loadedDataSize = 0
 	upperLeft := quadtree.Twof{+90, -180}
 	lowerRight := quadtree.Twof{-90, +180}
-	collector.tree = quadtree.MakeQuadtree(upperLeft, lowerRight)
+	srtmTileCollector.tree = quadtree.MakeQuadtree(upperLeft, lowerRight)
 }
 
 func loadHgtFilesIntoStorage(basePath string) {
